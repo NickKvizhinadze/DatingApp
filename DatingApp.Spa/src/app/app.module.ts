2a7fed420151a89036ffd7af128f4cb1e57b942e
NickKvizhinadze <nickkvizhinadze@gmail.com>
2018-05-25 17:16:05 +0400

    Initialize Spa(angular) project

---
+++ DatingApp.Spa/src/app/app.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { ValueComponent } from './value/value.component';
+
+@NgModule({
+  declarations: [AppComponent, ValueComponent],
+  imports: [BrowserModule, HttpModule],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
