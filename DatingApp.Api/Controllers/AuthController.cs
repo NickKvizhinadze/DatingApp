2a7962c8f9b98ca932ae99212502ad6a3881a917
NickKvizhinadze <nickkvizhinadze@gmail.com>
2018-05-30 09:46:17 +0400

    Added registration and authorization to api

---
+++ DatingApp.Api/Controllers/AuthController.cs
@@ -0,0 +1,77 @@
+﻿using DatingApp.Api.Data;
+using DatingApp.Api.Dtos;
+using DatingApp.Api.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatingApp.Api.Controllers
+{
+    [Route("api/[controller]")]
+    public class AuthController : Controller
+    {
+        private readonly IAuthRepository _repo;
+        private readonly IConfiguration _configuration;
+
+        public AuthController(IAuthRepository repo, IConfiguration configuration)
+        {
+            _repo = repo;
+            _configuration = configuration;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody]UserRegistrationDto model)
+        {
+            model.Username = model.Username.ToLower();
+            if (await _repo.UserExists(model.Username))
+                ModelState.AddModelError("", "Username is already taken");
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+
+            var userToCreate = new User
+            {
+                Username = model.Username
+            };
+
+            var createUser = await _repo.Register(userToCreate, model.Password);
+
+            return StatusCode(201);
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody]UserLoginDto model)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var existingUser = await _repo.Login(model.Username.ToLower(), model.Password);
+            if (existingUser == null)
+                return Unauthorized();
+
+            //TODO: Generate token
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_configuration.GetSection("AppSettings:Token").Value);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[] {
+                    new Claim(ClaimTypes.NameIdentifier, existingUser.Id.ToString()),
+                    new Claim(ClaimTypes.Name, existingUser.Username)
+                }),
+                Expires = DateTime.Now.AddMinutes(1),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            var tokenString = tokenHandler.WriteToken(token);
+
+            return Ok(new { token = tokenString });
+        }
+    }
+}
